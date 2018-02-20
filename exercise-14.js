0c9d5d79a06a078805a0381b014e32ef897fe55d
asepridwan1989 <aseprmail@gmail.com>
2018-02-20 10:10:56 +0700

    upload

--- exercise-14.js
+++ exercise-14.js
@@ -1,32 +1,18 @@
-function sorting(arrNumber) {
-  // code di sini
-  var length = arrNumber.length;
-  for (var i = 0; i < length; i++) { //Number of passes
-    for (var j = 0; j < (length - i - 1); j++) { //Notice that j < (length - i)
-      //Compare the adjacent positions
-      if(arrNumber[j] < arrNumber[j+1]) {
-        //Swap the numbers
-        var tmp = arrNumber[j];  //Temporary variable to hold the current number
-        arrNumber[j] = arrNumber[j+1]; //Replace current number with adjacent number
-        arrNumber[j+1] = tmp; //Replace adjacent number with current number
-      }
-    }
-  }
-  return arrNumber;
-}
-
 function getTotal(arrNumber) {
   // code di sini
+  if(arrNumber[0] === undefined){
+    return '';
+  }
   var length = arrNumber.length;
   var count = 1;
   for(var i = 1; i < length;i++){
     if(arrNumber[i] === arrNumber[0]){
-      count++
+      count++;
     }else{
-      return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + count +' kali'
+      return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + count +' kali';
     }
   }
-  return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + count +' kali'
+  return 'angka paling besar adalah ' + arrNumber[0] + ' dan jumlah kemunculan sebanyak ' + count +' kali';
 }
 
 function mostFrequentLargestNumbers(arrNumber) {
@@ -46,5 +32,5 @@ console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150])
 console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
 //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
 
-// console.log(countLargestNumbers([])); ----> error ga ada fungsinya
+console.log(mostFrequentLargestNumbers([]));
 //''
